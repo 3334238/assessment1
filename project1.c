6ab2e2cc4e0a7cc61701ecf6f20329483d30e1d5
3334238 <48506564+3334238@users.noreply.github.com>
2019-04-23 23:03:33 +1000

    finished (i think)

--- project1.c
+++ project1.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
-//task 3.2 code
+#include <string.h> //string library
+
 int main() {
-char message[25], ch; //
-int i, key;
-
-printf("enter a message to encrypt");
-
-printf("Enter key:");
-scanf("%d", &key);
-
-for(i = 0; message[i] !='\0'; ++i){
-    ch = message[i];
-    
-    if(ch >='a' &&ch <= 'z'){
-        ch = ch - key;
-        if(ch < 'a'){
-            ch = ch + 'z' - 'a' + 1;
-        }
-        
-        message [i] = ch;}
-
-  return 0;
-}
+char message[100], ch; //message string
+	int i, key;
+	
+	printf("Enter a message to encrypt: ");
+	gets(message);
+	printf("Enter key: ");
+	scanf("%d", &key);
+	
+	for(i = 0; message[i] != '\0'; i++){
+		ch = message[i];
+		if(ch >= 'A' && ch <= 'Z'){ //rotation for upper case letters
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1; //the actual rotation
+			}
+			
+			message[i] = ch;
+			printf("Encrypted message: %s", message); //prints encrypted message
+		}
+		
+		else if(ch >= 'a' && ch <= 'z'){ //rotation for lower case letters
+			ch = ch + key;
+			
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1; // have to rotate from upper case to lower case
+			}
+			
+			message[i] = ch;
+			printf("Encrypted message: %s", strlwr(message)); //strlwr function prints the encrypted message in capital letters
+		}
+		
+	}
+	
+	
+	
+	return 0;
+}         
